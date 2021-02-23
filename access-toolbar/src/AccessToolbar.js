bd003324a32f8d58eb3da9b4b853bc538f49fcbe
riteshjainabadkar <riteshjainabadkar@gmail.com>
2021-02-24 02:30:34 +0530

    Toolbar changes

--- access-toolbar/src/AccessToolbar.js
+++ access-toolbar/src/AccessToolbar.js
@@ -5,16 +5,38 @@ import './AccessToolbar.css';
 function AccessToolbar() {
   return (
     <div className="App">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <div className="dbAssistToolbar">
+        <a href="#" >Text-To-Speech</a>
+        <a href="#" >Speech-To-Text</a>
+
+        <a id="link1em" href="#" className="class1em" onClick={() => handleClick('link1em')}>A</a>
+        <a id="link12em" href="#" className="class12em" onClick={() => handleClick('link12em')}>A</a>
+        <a id="link17em" href="#" className="class17em" onClick={() => handleClick('link17em')}>A</a>
+      </div>
+
+      <p>This is plain text</p>
+      <h1>This is h1 text</h1>
+      <h2>This is h2 text</h2>
+      <h3>This is h3 text</h3>
+      <h4>This is h4 text</h4>
+
     </div>
   );
+}
+
+function handleClick(i) {
+  document.body.classList.remove('class1em');
+  document.body.classList.remove('class12em');
+  document.body.classList.remove('class17em');
+
+
+  document.getElementById("link1em").classList.remove('current');
+  document.getElementById("link12em").classList.remove('current');
+  document.getElementById("link17em").classList.remove('current');
+
+  document.body.classList.add(document.getElementById(i).className);
+  document.getElementById(i).classList.add('current');
 }
 
 export default AccessToolbar;
