d9f72e2395b9d58790e1d466ae48ad7ebdb978f9
Prashant <prashant.tale@live.com>
2021-02-23 22:32:45 +0530

    adding files

---
+++ access-toolbar/src/AccessToolbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './AccessToolbar.css';
+
+function AccessToolbar() {
+  return (
+    <div className="App">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </div>
+  );
+}
+
+export default AccessToolbar;
