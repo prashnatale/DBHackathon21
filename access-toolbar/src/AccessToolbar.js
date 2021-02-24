ad3ebda7faff8dad5ad73697ab69703b295eb5d8
riteshjainabadkar <riteshjainabadkar@gmail.com>
2021-02-24 23:25:10 +0530

    changes adding client application

--- access-toolbar/src/AccessToolbar.js
+++ access-toolbar/src/AccessToolbar.js
@@ -4,22 +4,16 @@ import './AccessToolbar.css';
 
 function AccessToolbar() {
   return (
-    <div className="App">
+    <div className="dbAssistToolbar">
 
-      <div className="dbAssistToolbar">
-        <a href="#" >Text-To-Speech</a>
-        <a href="#" >Speech-To-Text</a>
+      {/* <div className="dbAssistToolbar"> */}
+      <a href="#" >Text-To-Speech</a>
+      <a href="#" onClick={() => gettext()}>Speech-To-Text</a>
 
-        <a id="link1em" href="#" className="class1em" onClick={() => handleClick('link1em')}>A</a>
-        <a id="link12em" href="#" className="class12em" onClick={() => handleClick('link12em')}>A</a>
-        <a id="link17em" href="#" className="class17em" onClick={() => handleClick('link17em')}>A</a>
-      </div>
-
-      <p>This is plain text</p>
-      <h1>This is h1 text</h1>
-      <h2>This is h2 text</h2>
-      <h3>This is h3 text</h3>
-      <h4>This is h4 text</h4>
+      <a id="link1em" href="#" className="class1em" onClick={() => handleClick('link1em')}>A</a>
+      <a id="link12em" href="#" className="class12em" onClick={() => handleClick('link12em')}>A</a>
+      <a id="link17em" href="#" className="class17em" onClick={() => handleClick('link17em')}>A</a>
+      {/* </div> */}
 
     </div>
   );
@@ -37,6 +31,10 @@ function handleClick(i) {
 
   document.body.classList.add(document.getElementById(i).className);
   document.getElementById(i).classList.add('current');
+}
+
+function gettext() {
+  alert(document.body.innerText);
 }
 
 export default AccessToolbar;
---
+++ demo-project/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import AccessToolbar from 'accesstoolbar-hackathon-demo/lib/AccessToolbar'
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* <div className="dbAssistToolbar">
+        <a href="#" >Text-To-Speech</a>
+        <a href="#" >Speech-To-Text</a>
+
+        <a id="link1em" href="#" className="class1em" onClick={() => handleClick('link1em')}>A</a>
+        <a id="link12em" href="#" className="class12em" onClick={() => handleClick('link12em')}>A</a>
+        <a id="link17em" href="#" className="class17em" onClick={() => handleClick('link17em')}>A</a>
+      </div>*/}
+      <AccessToolbar></AccessToolbar>
+
+      <p>This is plain text</p>
+      <h1>This is h1 text</h1>
+      <h2>This is h2 text</h2>
+      <h3>This is h3 text</h3>
+      <h4>This is h4 text</h4>
+
+
+
+    </div>
+  );
+}
+
+function handleClick(i) {
+  document.body.classList.remove('class1em');
+  document.body.classList.remove('class12em');
+  document.body.classList.remove('class17em');
+
+
+  document.getElementById("link1em").classList.remove('current');
+  document.getElementById("link12em").classList.remove('current');
+  document.getElementById("link17em").classList.remove('current');
+
+  document.body.classList.add(document.getElementById(i).className);
+  document.getElementById(i).classList.add('current');
+}
+
+export default App;
